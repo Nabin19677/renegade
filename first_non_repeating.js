5c4089a5064555792178ca67552ab9bcbfc4d547
Anil Khadka <anil.khadka3777@gmail.com>
2022-04-30 21:36:25 +0545

    fix

--- first_non_repeating.js
+++ first_non_repeating.js
@@ -1,7 +1,7 @@
 exports.firstNonRepeatingCharacter = (str) => {
   for (let i = 0; i < str.length; i++) {
     if (str.indexOf(str.charAt(i)) == str.lastIndexOf(str.charAt(i))) {
-      return str.indexOf(str.charAt(i));
+      return i;
     }
   }
   return -1;
